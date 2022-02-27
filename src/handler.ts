716621086b476b98bf1a1aaae41f7c45c6ae4c31
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2022-02-27 14:48:58 +0900

    test deploy

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
 export async function handleRequest(request: Request): Promise<Response> {
   const url = 'https://github.com/Tak-Iwamoto'
   const res = await fetch(url)
-  return new Response(res.body, {
+  return new Response('test', {
     headers: { 'content-type': 'text/html' },
   })
 }
